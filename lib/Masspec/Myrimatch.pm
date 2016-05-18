66f37d381972a82af68f33ac6a27f3af2c044257
Pan Zhang <pan.zhang@vanderbilt.edu>
2016-05-18 11:37:16 -0500

    add not versioned file

---
+++ lib/Masspec/Myrimatch.pm
@@ -0,0 +1,117 @@
+#!/usr/bin/perl
+package Masspec::Myrimatch;
+
+use strict;
+use warnings;
+use File::Basename;
+use CQS::PBS;
+use CQS::ConfigUtils;
+use CQS::SystemUtils;
+use CQS::FileUtils;
+use CQS::Task;
+use CQS::NGSCommon;
+use CQS::StringUtils;
+
+our @ISA = qw(CQS::Task);
+
+sub new {
+  my ($class) = @_;
+  my $self = $class->SUPER::new();
+  $self->{_name}   = "Myrimatch";
+  $self->{_suffix} = "_mm";
+  bless $self, $class;
+  return $self;
+}
+
+sub perform {
+  my ( $self, $config, $section ) = @_;
+
+  my ( $task_name, $path_file, $pbsDesc, $target_dir, $logDir, $pbsDir, $resultDir, $option, $sh_direct ) = get_parameter( $config, $section );
+  my $proteindb = $config->{$section}{proteindb} or die "define ${section}::proteindb first";
+  my %mgffiles = %{ get_raw_files( $config, $section)};
+  
+
+  my $shfile = $self->taskfile( $pbsDir, $task_name );
+  open( SH, ">$shfile" ) or die "Cannot create $shfile";
+  print SH get_run_command($sh_direct);
+
+  my $threadcount = get_pbs_thread( $config->{$section}{pbs} );
+  my @isotopes;
+  if ( $config->{$section}{N15} == 1) {
+  	@isotopes=("N14","N15"); 
+  }
+  else {
+  	@isotopes=("N14");
+  }
+
+  for my $sampleName ( sort keys %mgffiles ) {
+    my @sampleFiles = @{ $mgffiles{$sampleName} };
+    my $samples = join( " ", @sampleFiles );
+
+    my $pbsFile = $self->pbsfile( $pbsDir, $sampleName );
+    $pbsFile = substr($pbsFile,0,-4);
+    my $log     = $self->logfile( $logDir, $sampleName );
+    $log = substr($log,0,-4);
+    
+    
+    foreach (@isotopes) {
+    	my $realpbsfile = $pbsFile."_".$_.".pbs";
+    	my $reallog = $log."_".$_.".log";
+    	my $pbsName = basename($realpbsfile);
+    	my $whichcfg = "cfgfile_".$_;
+    	my $cfgfile = $config->{$section}{$whichcfg} or die "define ${section}::cfgfile first";
+    
+
+
+    open( OUT, ">$realpbsfile" ) or die $!;
+
+    print OUT "$pbsDesc
+#SBATCH --output=$reallog
+
+$path_file
+
+cd $resultDir 
+
+
+
+echo myrimatch_start=`date` 
+
+myrimatch -cfg $cfgfile -workdir $resultDir -cpus $threadcount -ProteinDatabase $proteindb $samples
+
+echo finished=`date` 
+
+";
+    close(OUT);
+
+    print SH "\$MYCMD ./$pbsName \n";
+    print "$realpbsfile created\n";
+  }
+  }  
+  print SH "exit 0\n";
+  close(SH);
+
+  if ( is_linux() ) {
+    chmod 0755, $shfile;
+  }
+
+  print "!!!shell file $shfile created, you can run this shell file to submit all tasks.\n";
+}
+
+sub result {
+  my ( $self, $config, $section, $pattern ) = @_;
+
+  my ( $task_name, $path_file, $pbsDesc, $target_dir, $logDir, $pbsDir, $resultDir, $option, $sh_direct ) = get_parameter( $config, $section );
+
+  my %rawFiles = %{ get_raw_files( $config, $section ) };
+  
+
+  my $result = {};
+  for my $sampleName ( keys %rawFiles ) {
+    my @resultFiles = ();
+    
+    $result->{$sampleName} = filter_array( \@resultFiles, $pattern );
+  }
+  return $result;
+}
+
+1;
---
+++ lib/Proteomics/Engine/Myrimatch.pm
@@ -0,0 +1,110 @@
+#!/usr/bin/perl
+package Proteomics::Engine::Myrimatch;
+
+use strict;
+use warnings;
+use File::Basename;
+use CQS::PBS;
+use CQS::ConfigUtils;
+use CQS::SystemUtils;
+use CQS::FileUtils;
+use CQS::Task;
+use CQS::NGSCommon;
+use CQS::StringUtils;
+
+our @ISA = qw(CQS::Task);
+
+sub new {
+  my ($class) = @_;
+  my $self = $class->SUPER::new();
+  $self->{_name}   = "Proteomics::Engine::Myrimatch";
+  $self->{_suffix} = "_mm";
+  bless $self, $class;
+  return $self;
+}
+
+sub perform {
+  my ( $self, $config, $section ) = @_;
+
+  my ( $task_name, $path_file, $pbsDesc, $target_dir, $logDir, $pbsDir, $resultDir, $option, $sh_direct, $cluster, $thread ) = get_parameter( $config, $section );
+  my $database = $config->{$section}{database} or die "define ${section}::proteindb first";
+  my %mgffiles = %{ get_raw_files( $config, $section)};
+  my $executable = $config->{$section}{executable};
+  my $cfgfile = $config->{$section}{cfgfile};
+  
+
+  my $shfile = $self->taskfile( $pbsDir, $task_name );
+  open( SH, ">$shfile" ) or die "Cannot create $shfile";
+  print SH get_run_command($sh_direct);
+
+  for my $sampleName ( sort keys %mgffiles ) {
+    my @sampleFiles = @{ $mgffiles{$sampleName} };
+    my $pbsFile = $self->pbsfile( $pbsDir, $sampleName );
+    my $pbsName = basename($pbsFile);
+    my $log     = $self->logfile( $logDir, $sampleName );
+    my $log_desc = $cluster->get_log_desc($log);
+    
+
+    open( OUT, ">$pbsFile" ) or die $!;
+
+    print OUT "$pbsDesc
+$log_desc
+
+$path_file
+
+cd $resultDir 
+";
+
+	for my $sampleFile (@sampleFiles) {
+	      my $sname = basename($sampleFile);
+#	      my $resultFile = change_extension( $sname, ".myrimatch.txt" );
+	
+	      print OUT "
+myrimatch -cfg $cfgfile -workdir $resultDir -cpus $thread -ProteinDatabase $database $sampleFile
+	";
+	}
+
+    print OUT "
+echo finished=`date` 
+
+exit 0
+";
+    close(OUT);
+
+    print SH "\$MYCMD ./$pbsName \n";
+    print "$pbsFile created\n";
+  }  
+  print SH "exit 0\n";
+  close(SH);
+
+  if ( is_linux() ) {
+    chmod 0755, $shfile;
+  }
+
+  print "!!!shell file $shfile created, you can run this shell file to submit all tasks.\n";
+}
+
+sub result {
+  my ( $self, $config, $section, $pattern ) = @_;
+
+  my ( $task_name, $path_file, $pbsDesc, $target_dir, $logDir, $pbsDir, $resultDir, $option, $sh_direct ) = get_parameter( $config, $section );
+
+  my %rawFiles = %{ get_raw_files( $config, $section ) };
+  
+
+  my $result = {};
+  for my $sampleName ( keys %rawFiles ) {
+    my @resultFiles = ();
+    my @sampleFiles = @{ $rawFiles{$sampleName}};
+    for my $sampleFile (@sampleFiles) {
+    	my $sname=basename($sampleFile);
+    	my $resultFile=change_extension($sname,".myrimatch.pepXML");
+    	push(@resultFiles, "${resultDir}/${resultFile}");
+    }
+    
+    $result->{$sampleName} = filter_array( \@resultFiles, $pattern );
+  }
+  return $result;
+}
+
+1;
